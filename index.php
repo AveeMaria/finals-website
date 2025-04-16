4d2915d712ccc30b994d734cb9ded6d59df508f8
AveeMaria <gasper.muhvic@protonmail.com>
2025-04-16 21:21:17 +0200

    pagination podatkov

--- index.php
+++ index.php
@@ -1,21 +1,25 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="si">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>Podatki o igri!</title>
+    <title>Vojna Kraljestev - Podatki</title>
 </head>
 <body>
     <div class="container">
-        <h1>Game Log</h1>
+        <h1>Podatki o igrah</h1>
+        <div id="controls-div">
+            <button class="btn" id="prev">Nazaj</button>
+            <button class="btn" id="next">Naprej</button>
+        </div>
         <table>
             <thead>
                 <tr>
-                    <th>Attacker IP</th>
-                    <th>Defender IP</th>
-                    <th>Date</th>
-                    <th>Time</th>
+                    <th>IP Napadalca</th>
+                    <th>IP Branilca</th>
+                    <th>Datum</th>
+                    <th>Čas</th>
                 </tr>
             </thead>
             <tbody id="tdata"></tbody>
@@ -24,11 +28,33 @@
 </body>
 <script src="fetch.js"></script>
 <script>
+let currPage = 0;
 
-window.onload = async () => {
-    const data = await fetchData(10, 0);
+const prevBtn = document.getElementById('prev').addEventListener("click", function() {
+    if(currPage >= 1) {
+        currPage--;
+        loadPage();
+    }
+});
+
+const nextBtn = document.getElementById('next').addEventListener("click", function() {
+    currPage++;
+    loadPage();
+});
+
+window.onload = loadPage();
+
+async function loadPage() {
+    const data = await fetchData(10, currPage * 10);
+
+    if (!data || data.length === 0) {
+        currPage--;
+        return;
+    }
+
 
     const tdata = document.getElementById('tdata');
+    tdata.innerHTML = '';
 
     data.forEach((row) => {
         tdata.innerHTML += `
@@ -40,8 +66,7 @@ window.onload = async () => {
         </tr>
         `;
     });
-};
-
+}
 
 </script>
 </html>
